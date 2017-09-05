c7ed225005ab81f6508099b9ecbbf9745c5f1629
Harrison Lingren <harrisonlingren@gmail.com>
2017-09-04 21:34:39 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, render_template, abort
+from flask import Flask, request, redirect, render_template, abort, url_for
 from urllib.parse import urlparse
 import links
 
